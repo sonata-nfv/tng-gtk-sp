5a81741ebba4c1e559edeafceb221fdb60733ffa
José Bonnet <jose.bonnet@icloud.com>
2019-05-10 09:31:50 +0100

    Fix... again!

--- services/process_scale_service_instance_request.rb
+++ services/process_scale_service_instance_request.rb
@@ -105,7 +105,7 @@ class ProcessScaleServiceInstanceRequest < ProcessRequestBase
     return {error: "The number of instances must be greater than 0 (defaults to one, if absent)"} if (params.key?(:number_of_instances) && params[:number_of_instances] < 1)
     return {error: "The VNFD UUID must be present"} unless params.key?(:vnfd_uuid)
     #return {error: "The VNFD UUID must be valid"} unless uuid_valid?(:vnfd_uuid)
-    return {error: "The VIM UUID must be valid"} if (params[:scaling_type].upcase == "ADD_VNF" && params.key?(:vim_uuid) && !Tng::Gtk::Utils.uuid_valid?(:vim_uuid))
+    return {error: "The VIM UUID must be valid"} if (params[:scaling_type].upcase == "ADD_VNF" && params.key?(:vim_uuid) && !uuid_valid?(:vim_uuid))
     {}
   end
   
@@ -135,11 +135,6 @@ class ProcessScaleServiceInstanceRequest < ProcessRequestBase
     msg='.'+__method__.to_s
     LOGGER.debug(component:LOGGED_COMPONENT, operation:msg, message:"params=#{params}")
     params
-  end
-  
-  def uuid_valid?(uuid)
-    return true if (uuid =~ /[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}/) == 0
-    false
   end
   
   def uuid_valid?(uuid)
