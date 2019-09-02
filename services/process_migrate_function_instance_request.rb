40121a1d15cc21a7b6d5b1941fa305e06003e5a6
José Bonnet <jose.bonnet@icloud.com>
2019-09-02 15:18:22 +0100

    Add v0 of function migration

--- services/process_migrate_function_instance_request.rb
+++ services/process_migrate_function_instance_request.rb
@@ -61,25 +61,24 @@ class ProcessMigrateFunctionInstanceRequest < ProcessRequestBase
     completed_params = complete_params(params)
     LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"completed params=#{completed_params}")
     begin
-      scaling_request = Request.create(completed_params).as_json
+      migration_request = Request.create(completed_params).as_json
     ensure
       Request.clear_active_connections!
     end
-    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"scaling_request=#{scaling_request} (class #{scaling_request.class})")
-    unless scaling_request
-      LOGGER.error(component:LOGGED_COMPONENT, operation: msg, message:"Failled to create scaling request for service instance '#{params[:instance_uuid]}'")
-      return {error: "Failled to create the scale request #{params}"}
+    unless migration_request
+      LOGGER.error(component:LOGGED_COMPONENT, operation: msg, message:"Failled to create migration request for function instance '#{params[:instance_uuid]}'")
+      return {error: "Failled to create the migration request #{params}"}
     end
-    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"scaling_request=#{scaling_request}")
-    message = build_message(completed_params[:scaling_type], completed_params[:instance_uuid], completed_params[:vnfd_uuid], completed_params[:number_of_instances], completed_params[:vim_uuid])
+    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"migration_request=#{migration_request}")
+    message = build_message(completed_params[:instance_uuid], completed_params[:vnf_uuid], completed_params[:vim_uuid])
     begin
-     published_response = MessagePublishingService.call(message, :migrate_function, scaling_request[:id])
+     published_response = MessagePublishingService.call(message, :migrate_function, migration_request[:id])
      LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"published_response=#{published_response}")
     rescue StandardError => e
       LOGGER.error(component:LOGGED_COMPONENT, operation: msg, message:"(#{e.class}) #{e.message}\n#{e.backtrace.split('\n\t')}")
       return {error: "#{LOGGED_COMPONENT}#{msg} (#{__LINE__}):\n#{e.message}\n#{e.backtrace.split('\n\t')}"}
     end
-    scaling_request
+    migration_request
   end
   
   def self.enrich_one(request)
@@ -101,42 +100,37 @@ payload:
 service_instance_uuid: <id of the service instance>
 vnf_uuid: <id of the vnf instance that needs to be migrated>
 vim_uuid: <id of the vim that the vnf needs to be migrated to>
+data = {
+            "instance_uuid": instance_uuid,
+            "request_type": "MIGRATE_FUNCTION",
+            "vnf_uuid": vnf_uuid,
+            "vim_uuid": vim_uuid
+        }
 =end
     msg='.'+__method__.to_s
     LOGGER.debug(component:LOGGED_COMPONENT, operation:msg, message:"params=#{params}")
 
-    return {error: "The type of scaling must be present"} unless params.key?(:scaling_type)
-    return {error: "The type of scaling must be either ADD_VNF or REMOVE_VNF"} unless (params[:scaling_type].upcase == "ADD_VNF" || params[:scaling_type].upcase == "REMOVE_VNF")
     return {error: "The service instance UUID must be present"} unless params.key?(:instance_uuid)
     return {error: "The service instance UUID must be valid"} unless uuid_valid?(params[:instance_uuid])
-    return {error: "Either the VNFD UUID or the VNF UUID must be present on a scale in (REMOVE_VNF)"} if (params[:scaling_type].upcase == "REMOVE_VNF" && !params.key?(:vnfd_uuid) && !params.key?(:vnf_uuid))
-    return {error: "The VNFD UUID must be present"} if (params[:scaling_type].upcase == "ADD_VNF" && !params.key?(:vnfd_uuid))
-    return {error: "The VNFD UUID must be valid"} if (params.key?(:vnfd_uuid) && !uuid_valid?(params[:vnfd_uuid]))
-    return {error: "The VNF UUID must be valid"} if (params.key?(:vnf_uuid) && !uuid_valid?(params[:vnf_uuid]))
-    return {error: "The number of instances must be greater than 0 (defaults to one, if absent)"} if (params.key?(:number_of_instances) && params[:number_of_instances].to_i < 1)
-    return {error: "The VIM UUID must be valid"} if (params[:scaling_type].upcase == "ADD_VNF" && params.key?(:vim_uuid) && !uuid_valid?(params[:vim_uuid]))
+    return {error: "The VNF UUID must be present"} unless params.key?(:vnf_uuid)
+    return {error: "The VNF UUID must be valid"} unless uuid_valid?(params[:vnf_uuid])
+    return {error: "The VIM UUID must be present"} unless params.key?(:vim_uuid)
+    return {error: "The VIM UUID must be valid"} unless uuid_valid?(params[:vim_uuid])
     {}
   end
   
   def complete_params(params)
-    new_params = params.dup
-    new_params[:vim_uuid] = params.fetch(:vim_uuid, '')
-    new_params[:number_of_instances] = params.fetch(:number_of_instances, 1)
-    new_params
+    params
   end
   
-  def build_message(scaling_type, instance_uuid, vnfd_uuid, number_of_instances, vim_uuid)
+  def build_message(instance_uuid, vnf_uuid, vim_uuid)
     msg='.'+__method__.to_s
-    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"scaling_type=#{scaling_type} instance_uuid=#{instance_uuid} vnfd_uuid=#{vnfd_uuid}")
+    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"instance_uuid=#{instance_uuid} vnf_uuid=#{vnf_uuid} vim_uuid=#{vim_uuid}")
     message = {}
-    message['scaling_type'] = scaling_type
     message['service_instance_uuid'] = instance_uuid
-    message['vnfd_uuid'] = vnfd_uuid
-    message['number_of_instances'] = number_of_instances.to_i
-    if vim_uuid
-      message['constraints'] = {'vim_uuid'=>vim_uuid}
-    end
-    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"message=#{message}")
+    message['vnf_uuid'] = vnf_uuid
+    message['vim_uuid'] = vim_uuid
+    LOGGER.debug(component:LOGGED_COMPONENT, operation: msg, message:"message=#{message}"
     message.to_yaml.to_s
   end  
   
